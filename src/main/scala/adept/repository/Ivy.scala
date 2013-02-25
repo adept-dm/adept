1d83529aebc8e33b8f869de6b73d4e13c7fa6b29
Fredrik Ekholdt <fredrik.ekholdt@typesafe.com>
2013-02-25 14:29:08 +0100

    removed ivy and location which are just junk

--- src/main/scala/adept/repository/Ivy.scala
+++
@@ -1,9 +0,0 @@
-package adept.repository
-
-import java.io.{File => jFile}
-
-object Ivy {
-  def add(descriptor: Coordinates, settings: jFile) = {
-    throw RemoveThisNotImplementedException
-  }
-}
