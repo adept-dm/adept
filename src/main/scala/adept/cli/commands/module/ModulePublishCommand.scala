e63dea24f80c52e98866ef8e547fb9cc485eb0e5
Tomas Herman <tomas.herman@gmail.com>
2013-05-26 21:36:40 +0200

    publish adds module into local adept repo and commits it

--- src/main/scala/adept/cli/commands/module/ModulePublishCommand.scala
+++ src/main/scala/adept/cli/commands/module/ModulePublishCommand.scala
@@ -1,13 +1,21 @@
 package adept.cli.commands.module
 
+import adept.Adept
 import adept.cli.commands.Command
 
-object ModulePublishCommand extends Command {
+object ModulePublishCommand extends Command with JsonFileSystemModulePersistance {
 
   val command = "publish"
   val shortDescription = "publish current module to the world"
 
   def execute(args: List[String]): CommandResult = {
-    Right(Option("publish"))
+    for {
+      adept <- Adept.open(new java.io.File(".adept"), "local").right
+      module <- loadModule.right
+    } yield  {
+      adept.add(module)
+      adept.commit("published")
+      None
+    }
   }
 }
