04d4db67a10fff8eccc38ceaa61d9f49c0481d17
Fredrik Ekholdt <fredrik.ekholdt@typesafe.com>
2013-07-30 12:15:12 +0200

    fixes ivytest

--- adept-core/src/test/scala/adept/ivy/IvyTest.scala
+++ adept-core/src/test/scala/adept/ivy/IvyTest.scala
@@ -6,14 +6,18 @@ import adept.core.models._
 import org.apache.ivy.core.IvyContext
 import java.io.File
 import adept.core.Adept
+import org.eclipse.jgit.util.FileUtils
 
 class IvyTest extends FunSuite with MustMatchers {
 
   test("ivy basic") {
+    import org.scalatest.EitherValues._
     val ivy = IvyContext.getContext.getIvy //TODO: is this right?
 
-    ivy.configure(new File("../typesafe-ivy-settings.xml"))
-    val adeptDir = new File("/Users/freekh/.adept")
+    ivy.configure(new File("adept-core/src/test/resources/typesafe-ivy-settings.xml"))
+    val adeptDir = new File("tests/localrepo")
+    try {
+      
     val testRepo = "local"
     val adept = Adept.open(adeptDir, testRepo) match {
       case Left(_) => Adept.init(adeptDir, testRepo) match {
@@ -21,6 +25,12 @@ class IvyTest extends FunSuite with MustMatchers {
       }
       case Right(adept) => adept
     }
-    IvyImport.add(Coordinates("play", "play_2.10", "2.1.2"), ivy, adept)
+    val coords = Coordinates("com.typesafe.akka", "akka-actor_2.10", "2.1.2")
+    IvyImport.add(coords, ivy, adept) must have size(3)
+    
+    adept.findModule(coords, None, Set.empty).right.value
+    } finally {
+      FileUtils.delete(adeptDir, FileUtils.RECURSIVE)
+    }
   }
 }
