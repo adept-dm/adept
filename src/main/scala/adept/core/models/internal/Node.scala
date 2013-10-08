f3d44b4c0d6a77c132db10d148afdfa4bfe6c1ba
Fredrik Ekholdt <freekh@gmail.com>
2013-10-08 20:39:59 +0200

    relaxing private scope to make it testable

--- src/main/scala/adept/core/models/internal/Node.scala
+++ src/main/scala/adept/core/models/internal/Node.scala
@@ -1,6 +1,6 @@
 package adept.core.models.internal
 
-private[core] case class Node(val id: String, var children: Set[Node]) {
+private[adept] case class Node(val id: String, var children: Set[Node]) {
   override def toString = {
     id + " <children>"
   }
