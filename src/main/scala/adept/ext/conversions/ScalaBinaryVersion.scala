6f0462cfe1cb240a8054e4b9c4ec1cee867c6c06
Fredrik Ekholdt <freekh@gmail.com>
2014-02-05 11:59:58 +0100

    Fixed scala binary version conversion and added test

--- src/main/scala/adept/ext/conversions/ScalaBinaryVersion.scala
+++ src/main/scala/adept/ext/conversions/ScalaBinaryVersion.scala
@@ -6,21 +6,22 @@ import adept.ext.AttributeDefaults
 
 object ScalaBinaryVersion extends Conversion {
 
-  private lazy val ScalaLibBinaryVersionRegEx = """.*_(\d\.\d{2})$""".r
-  private lazy val ScalaLibId = Id("scala-library")
+  private lazy val ScalaLibBinaryVersionRegEx = """(.*)_(\d\.\d{2})$""".r
+  val ScalaLibId = Id("scala-library")
 
   def convert(configuredVariant: ConfiguredVariantsMetadata): Option[ConfiguredVariantsMetadata] = {
     configuredVariant.id.value match {
-      case ScalaLibBinaryVersionRegEx(scalaBinaryVersion) => {
+      case ScalaLibBinaryVersionRegEx(newId, scalaBinaryVersion) => {
+        
         val configurations = configuredVariant.configurations.map { configuration =>
           val requirements = configuration.requirements.map { requirement =>
-            if (requirement.id == ScalaLibId) {
+            if (requirement.id == ScalaLibId && requirement.constraints.find(_.name == AttributeDefaults.BinaryVersionAttribute).isEmpty) { //if scala lib and binary version is not set
               requirement.copy(constraints = requirement.constraints + Constraint(AttributeDefaults.BinaryVersionAttribute, Set(scalaBinaryVersion)))
             } else requirement
           }
           configuration.copy(requirements = requirements)
         }
-        Some(configuredVariant.copy( configurations = configurations ))
+        Some(configuredVariant.copy( id = Id(newId), configurations = configurations ))
       }
       case _ => Some(configuredVariant)
     }
--- src/test/scala/adept/ext/conversions/ScalaBinaryVersionTest.scala
+++ src/test/scala/adept/ext/conversions/ScalaBinaryVersionTest.scala
@@ -1,10 +1,44 @@
 package adept.ext.conversions
 
-import org.scalatest.FunSuite
+import org.scalatest._
 import org.scalatest.matchers.MustMatchers
+import adept.models._
+import adept.repository.models._
+import adept.repository.models.configuration._
+import adept.ext.AttributeDefaults
 
 class ScalaBinaryVersionTest extends FunSuite with MustMatchers {
+
   test("scala binary version conversion correctness") {
-   
+    import OptionValues._
+    val akkaName = "akka-actor"
+    val scalaVersion = "2.10"
+    val configId = Id("config")
+
+    val compileConf = Configuration(
+      id = ConfigurationId("compile"),
+      extendsConfigurations = Set.empty,
+      metadata = Set.empty[MetadataInfo],
+      artifacts = Set.empty,
+      attributes = Set.empty,
+      requirements = Set(ConfiguredRequirement(ScalaBinaryVersion.ScalaLibId, Set(ConfigurationId("compile"), ConfigurationId("master")),
+        constraints = Set.empty,
+        commits = Set.empty),
+        ConfiguredRequirement(configId, Set(ConfigurationId("compile"), ConfigurationId("master")),
+          constraints = Set.empty,
+          commits = Set.empty)))
+
+    val configuredVariant = ConfiguredVariantsMetadata(
+      id = Id(akkaName + "_" + scalaVersion),
+      metadata = Set.empty[MetadataInfo],
+      attributes = Set(Attribute("version", Set("2.2.1"))),
+      configurations = Set(
+        compileConf))
+
+    val converted = ScalaBinaryVersion.convert(configuredVariant).value
+    converted.id must be === Id(akkaName)
+    val configuration = converted.configurations.headOption.value
+    configuration.requirements.find(_.id === ScalaBinaryVersion.ScalaLibId).flatMap(_.constraints.find(_.name == AttributeDefaults.BinaryVersionAttribute)) must be === Some(Constraint(AttributeDefaults.BinaryVersionAttribute, Set(scalaVersion)))
+    configuration.requirements.find(_.id === configId).flatMap(_.constraints.find(_.name == AttributeDefaults.BinaryVersionAttribute)) must be === None
   }
 }
