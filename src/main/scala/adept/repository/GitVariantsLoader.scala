d4d128567ee9491f6c22670b92110fd6631727a1
Fredrik Ekholdt <freekh@gmail.com>
2014-02-28 11:56:11 +0100

    Performance boosts (reduced play resolve time with 1 to 0.5 secs)

--- src/main/scala/adept/repository/GitVariantsLoader.scala
+++ src/main/scala/adept/repository/GitVariantsLoader.scala
@@ -9,7 +9,7 @@ import adept.repository.models.ConfiguredVariantsMetadata
 
 class GitVariantsLoader(commits: Set[(Id, AdeptCommit)], cacheManager: CacheManager) extends VariantsLoader {
 
-  private val thisUniqueId = Hash.calculate(commits.map{ case (id, c) => id.value + c.commit.value + c.repo.name }.toSeq.sorted.mkString).value
+  private val thisUniqueId = Hash.calculate(commits.map { case (id, c) => id.value + c.commit.value + c.repo.name }.toSeq.sorted.mkString).value
 
   //we are using 1 cache here, so each time a commit is changed or something like that we invalidate this cache
   private val cache: Ehcache = {
@@ -18,20 +18,35 @@ class GitVariantsLoader(commits: Set[(Id, AdeptCommit)], cacheManager: CacheMana
     cacheManager.getEhcache(cacheName)
   }
 
+  def onlyCommits(id: Id) = {
+    var onlyCommits = Vector.empty[AdeptCommit] //perf boost (avoid multiple traversals)
+    commits.foreach { c =>
+      if (c._1 == id) onlyCommits +:= c._2
+    }
+    onlyCommits
+  }
+
+  def readVariants(lastCommitForId: Option[AdeptCommit]) = {
+    import collection.mutable._
+    var variants: Set[Variant] = new HashSet[Variant] with SynchronizedSet[Variant] //perf boost  par 
+    lastCommitForId.foreach { c =>
+      c.repo.listContent(c.commit.value).variantsMetadata.par.foreach { vm =>
+        variants ++= vm.toVariants
+      }
+    }
+    variants.toSet
+  }
+
   def loadVariants(id: Id, constraints: Set[Constraint]): Set[Variant] = {
     val cachedValues = cache.get(id.value)
     if (cache.isKeyInCache(id.value) && cachedValues != null) {
       cachedValues.getValue().asInstanceOf[Set[Variant]]
     } else {
-      val allVariants: Set[Variant] = Set() ++ {
-        val onlyCommits = commits.filter{ case (commitId, _) => commitId == id }.map(_._2).toSeq
-        //handle commit conflicts
-        val lastCommitForId = onlyCommits.toSeq.sorted.lastOption.toSet 
-        lastCommitForId.flatMap { c =>
-          c.repo.listContent(c.commit.value).variantsMetadata.flatMap(_.toVariants)
-        }
+      val allVariants: Set[Variant] = {
+        //handle commit conflicts choosing last commit
+        val lastCommitForId = onlyCommits(id).sorted.lastOption
+        readVariants(lastCommitForId)
       }
-//      if (id.value == "scala-library/config/compile") println("scala variants:" + commits)
       val filteredVariants = AttributeConstraintFilter.filter(id, allVariants, constraints)
       val element = new Element(id.value, filteredVariants)
       cache.put(element)
