99b16d90880a1feb265429797fd9d11dd65aceba
Fredrik Ekholdt <freekh@gmail.com>
2014-03-02 11:50:27 +0100

    Added meat to the sample

--- adept-sbt/sample/src/main/scala/test.scala
+++ adept-sbt/sample/src/main/scala/test.scala
@@ -1,3 +1,34 @@
 import akka._
 
 import play._
+
+import akka.actor.Actor
+import akka.actor.Props
+
+object Greeter {
+  case object Greet
+  case object Done
+}
+ 
+class Greeter extends Actor {
+  def receive = {
+    case Greeter.Greet ⇒
+      println("Hello World!")
+      sender ! Greeter.Done
+  }
+} 
+
+class HelloWorld extends Actor {
+ 
+  override def preStart(): Unit = {
+    // create the greeter actor
+    val greeter = context.actorOf(Props[Greeter], "greeter")
+    // tell it to perform the greeting
+    greeter ! Greeter.Greet
+  }
+ 
+  def receive = {
+    // when the greeter is done, stop this actor and with it the application
+    case Greeter.Done ⇒ context.stop(self)
+  }
+}
