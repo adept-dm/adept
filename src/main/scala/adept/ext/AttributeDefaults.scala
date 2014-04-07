cadb3342e277843f86af44d0976c7b2f46a28f93
Fredrik Ekholdt <freekh@gmail.com>
2014-04-07 14:43:09 +0200

    Added more attribute defaults

--- src/main/scala/adept/ext/AttributeDefaults.scala
+++ src/main/scala/adept/ext/AttributeDefaults.scala
@@ -3,4 +3,7 @@ package adept.ext
 object AttributeDefaults {
   val VersionAttribute = "version"
   val BinaryVersionAttribute = "binary-version"
+  val MatureAttribute = "mature"
+  val PrerelaseAttribute = "prerelease"
+  val SnapshotAttribute = "snapshot"
 }
