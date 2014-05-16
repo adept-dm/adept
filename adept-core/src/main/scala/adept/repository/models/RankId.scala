d808dfefa476b013a51558a89da937984ca94782
Fredrik Ekholdt <freekh@gmail.com>
2014-05-16 08:24:48 +0200

    Ordering rankings for sorting

--- adept-core/src/main/scala/adept/repository/models/RankId.scala
+++ adept-core/src/main/scala/adept/repository/models/RankId.scala
@@ -1,3 +1,11 @@
 package adept.repository.models
 
 case class RankId(value: String) extends AnyVal
+
+object RankId {
+  implicit val ordering =new Ordering[RankId] {
+    def compare(x: RankId, y: RankId): Int = {
+      x.value.compare(y.value)
+    }
+  }
+}
