85a752fec74d42e64d15d5af43a08710935768a3
Arve Knudsen <arve.knudsen@gmail.com>
2014-06-22 14:58:11 +0200

    Remove obsolete play-json format

--- adept-core/src/main/scala/adept/models/SearchResults.scala
+++ adept-core/src/main/scala/adept/models/SearchResults.scala
@@ -13,22 +13,3 @@ case class ImportSearchResult(override val variant: Variant, override val rankId
 case class GitSearchResult(override val variant: Variant, override val rankId: RankId,
   override val repository: RepositoryName, commit: Commit, locations: Seq[String], isLocal: Boolean = false)
   extends SearchResult(variant, rankId, repository, isImport = false)
-
-object GitSearchResult {
-//  implicit val format: Format[GitSearchResult] = {
-//    (
-//      (__ \ "id").format[String] and
-//      (__ \ "variant").format[VariantMetadata] and
-//      (__ \ "rank").format[String] and
-//      (__ \ "repository").format[String] and
-//      (__ \ "commit").format[String] and
-//      (__ \ "locations").format[Seq[String]])({
-//        case (id, variant, rank, repository, commit, locations) =>
-//         GitSearchResult(variant.toVariant(Id(id)), RankId(rank), RepositoryName(repository), Commit(commit), locations)
-//      },
-//        unlift({ gsr: GitSearchResult =>
-//          val GitSearchResult(variant, rank, repository, commit, locations, offline) = gsr
-//          Some(variant.id.value, VariantMetadata.fromVariant(variant), rank.value, repository.value, commit.value, locations)
-//        }))
-//  }
-}
